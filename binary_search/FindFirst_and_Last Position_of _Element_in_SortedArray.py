7db8ad0c0413f38a33f03d3ce6c7a6d229f375ea
yuhangxiaocs <38247819+yuhangxiaocs@users.noreply.github.com>
2020-03-23 23:51:22 +0800

    Create FindFirst_and_Last Position_of _Element_in_SortedArray.py

---
+++ binary_search/FindFirst_and_Last Position_of _Element_in_SortedArray.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if not nums: return [-1, -1]
+        lo, hi = 0, len(nums)-1
+        reslo = reshi = -1
+        while lo <= hi:
+            mid = (lo + hi)//2
+            if target <= nums[mid]:
+                hi = mid - 1
+            elif target > nums[mid] :
+                lo = mid + 1
+                
+        reslo = lo
+        lo, hi = 0, len(nums)-1
+        while lo <= hi:
+            mid = (lo + hi)//2
+            if target < nums[mid]:
+                hi = mid - 1
+            elif target >= nums[mid]:
+                lo = mid + 1        
+        reshi = hi        
+        return [reslo, reshi] if reslo <= reshi else [-1, -1]

2ca2c59ec95d75b6eb49487df63476f427735b1d
yuhangxiaocs <38247819+yuhangxiaocs@users.noreply.github.com>
2020-04-02 01:27:45 +0800

    three different versions

--- binary_search/FindFirst_and_Last Position_of _Element_in_SortedArray.py
+++ binary_search/FindFirst_and_Last Position_of _Element_in_SortedArray.py
@@ -1,3 +1,5 @@
+# 绝了 我这个版本真的不好理解 但还是对的...
+
 class Solution(object):
     def searchRange(self, nums, target):
         """
@@ -25,3 +27,80 @@ class Solution(object):
                 lo = mid + 1        
         reshi = hi        
         return [reslo, reshi] if reslo <= reshi else [-1, -1]
+    
+# 这个版本是参考一个YouTube的思路 它的想法很“笨” 但绝对不会错，那就是在每次mid==target的时候更新一个flag变量
+# 这样就可以让你忽略下标+1还是-1的问题，你只要确保不会miss中间的，以及循环可以正确退出即可
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = new int[2];
+        res[0] = findStarting(nums, target);
+        res[1] = findEnding(nums, target);
+        return res;
+    }
+    
+    public int findStarting(int[] nums, int target){
+        int lo = 0;
+        int hi = nums.length - 1;
+        int index = -1;
+        while (lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] >= target)
+                hi = mid - 1;
+            else
+                lo = mid + 1;
+                
+            if(nums[mid] == target)
+                index = mid;
+            
+        }
+        
+        
+        return index;
+    }
+    
+    public int findEnding(int[] nums, int target){
+        int lo = 0;
+        int hi = nums.length - 1;
+        int index = -1;
+        while (lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] > target)
+                hi = mid - 1;
+            else
+                lo = mid + 1;
+                
+            if(nums[mid] == target)
+                index = mid;
+            
+        }
+        
+        return index;
+        
+    }
+}
+
+# personally the best version
+
+class Solution(object):
+    def searchRange(self, nums, target):
+        def lower_bound(nums, target):
+            lo ,hi = 0, len(nums)
+            
+            while lo < hi:
+                mid = (lo + hi) // 2
+                if nums[mid] >= target: hi = mid
+                else: lo = mid + 1
+            return lo
+        
+        def upper_bound(nums, target):
+            lo ,hi = 0, len(nums)
+            while lo < hi:
+                mid = (lo + hi) // 2
+                if nums[mid] > target: hi = mid
+                else: lo = mid + 1
+            return lo
+        
+        lo, hi = lower_bound(nums, target), upper_bound(nums, target)
+        
+        return [lo, hi - 1] if lo != hi else [-1, -1]
